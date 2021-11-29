3a705a814b01a957d9cc4c133fa1d68779ced8e3
Lily <admin@mintlily.lgbt>
2021-11-28 21:33:06 -0500

    Change location of Auth Function

---
+++ MintMod/Functions/Authentication/AuthConstructor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using MintyLoader;
+using Newtonsoft.Json;
+using VRC.Core;
+
+namespace MintMod.Functions.Authentication {
+    public class MintyUser {
+        public string Name { get; }
+        //public string DiscordID { get; }
+        public string UserID { get; }
+        //public string VoucherName { get; }
+        public string[] AltAccounts { get; }
+        public bool isBanned { get; }
+    }
+}
---
+++ MintMod/Functions/Authentication/ServerAuth.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+using VRC.Core;
+using System.Net;
+using System.Net.Http;
+using MintyLoader;
+using MintMod.Utils;
+using Newtonsoft.Json;
+
+namespace MintMod.Functions.Authentication {
+    internal class ServerAuth : MintSubMod {
+        public override string Name => "Authentication";
+        public override string Description => "Deals with authed used for Mint.";
+
+        internal static bool canLoadMod;
+        internal const string MintAuthJSONURL = "https://api.potato.moe/api-mint/auth"; // From Bono's API
+        internal static MintyUser MintyData;
+
+        internal static IEnumerator AuthUser() {
+            yield return new WaitForSeconds(1);
+            while (true) {
+                if (APIUser.CurrentUser == null) yield return null;
+                else break;
+            }
+
+            try {
+                HttpClient w = new();
+                w.DefaultRequestHeaders.Add("X-AUTH-TOKEN", APIUser.CurrentUser.id);
+                var task = w?.GetStringAsync(MintAuthJSONURL);
+                task.Wait();
+                w?.Dispose();
+
+                if (!task.IsCompleted || task.Result.Contains("message")) {
+                    canLoadMod = false;
+                    Con.Debug("result was null or empty", MintCore.isDebug);
+                    yield break;
+                }
+                // else canLoadMod = true
+
+                MintyData = JsonConvert.DeserializeObject<MintyUser>(task.Result);
+
+                if (MintyData.isBanned) {
+                    canLoadMod = false;
+                    MelonCoroutines.Start(LoopNoAuth());
+                    yield break;
+                }
+
+                Con.Msg(ConsoleColor.Green, "Authed for MintMod");
+                canLoadMod = true;
+                MelonCoroutines.Start(GetAssembly.YieldUI());
+
+            } catch (Exception r) {
+                canLoadMod = false;
+                Con.Error($"{r}");
+                yield break;
+            }
+        }
+
+        static IEnumerator LoopNoAuth() {
+            while (true) {
+                Con.Warn("You are not authorized to use this mod.");
+                yield return new WaitForSeconds(60);
+            }
+        }
+
+        internal static IEnumerator SimpleAuthCheck(string id) {
+            HttpClient e = new();
+            e.DefaultRequestHeaders.Add("X-AUTH-TOKEN", id);
+            var task = e.GetStringAsync(MintAuthJSONURL);
+            task.Wait();
+            e.Dispose();
+
+            if (!task.IsCompleted || task.Result.Contains("message")) {
+                Con.Msg("Player has no auth for Mint");
+                VRCUiManager.prop_VRCUiManager_0.InformHudText("Player has no auth for Mint", Color.yellow);
+                yield break;
+            }
+
+            var d = JsonConvert.DeserializeObject<MintyUser>(task.Result);
+
+            if (d.isBanned) {
+                Con.Msg("Player is banned from Mint");
+                VRCUiManager.prop_VRCUiManager_0.InformHudText("Player is banned from Mint", Color.red);
+                yield break;
+            }
+            Con.Msg("Player is authed for Mint");
+            VRCUiManager.prop_VRCUiManager_0.InformHudText("Player is authed for Mint", Color.white);
+        }
+    }
+}
