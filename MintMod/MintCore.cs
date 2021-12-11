a64de899a6d9fb47dbc4afa153a7a7297e2fc13a
Lily <admin@mintlily.lgbt>
2021-12-11 14:58:15 -0500

    Mod v2.5.1 - Fix ML Outdated Goof

--- MintMod/MintCore.cs
+++ MintMod/MintCore.cs
@@ -29,13 +29,12 @@ namespace MintMod {
             public const string Name = "MintMod";
             public const string Author = "Lily";
             public const string Company = "LilyMod";
-            public const string Version = "2.5.0";
+            public const string Version = "2.5.1";
             public const string DownloadLink = null;
             public const string UpdatedDate = "11 Dec 2021";
 #if !DEBUG
             public const string LoaderVer = "2.3.0.1";
             public static Version TargetMLVersion = new(0, 5, 2);
-            public static Version AcceptedMLVersion = new(0, 4, 3);
 #endif
         }
 
@@ -65,7 +64,7 @@ namespace MintMod {
 #if !DEBUG
             if (Environment.CommandLine.Contains("--MintyDev")) isDebug = true;
             var melonVersion = new Version(BuildInfo.Version);
-            if (melonVersion != ModBuildInfo.TargetMLVersion || melonVersion != ModBuildInfo.AcceptedMLVersion) {
+            if (melonVersion != ModBuildInfo.TargetMLVersion) {
                 MessageBox.Show("Your MelonLoader is outdated.", "Outdated Mod Loader");
                 Process.Start("https://github.com/HerpDerpinstine/MelonLoader/releases/latest");
                 Con.Warn("Your MelonLoader version is out of date, please update it.");
