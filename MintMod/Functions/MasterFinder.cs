f1bbd372b54b3c9cf7f1bd45fe28a1e97e0d7fc0
Lily <admin@mintlily.lgbt>
2021-12-31 01:53:47 -0500

    Fix MasterFinder action process

--- MintMod/Functions/MasterFinder.cs
+++ MintMod/Functions/MasterFinder.cs
@@ -19,28 +19,27 @@ using MintMod.UserInterface.QuickMenu;
 namespace MintMod.Functions {
     class MasterFinder : MintSubMod {
         public override string Name => "MasterFinder";
-        public override string Description => "Shows a crown uptop the users's nameplate showing instance master.";
+        public override string Description => "Shows a crown on top the user's nameplate showing instance master.";
 
         public static GameObject MasterIcon, oriFriendMarker;
         private static RectTransform r;
 
         internal static void OnAvatarIsReady(VRCPlayer vrcPlayer) {
-            if (MintUserInterface.isOnStreamerMode) return;
+            if (MintUserInterface.isStreamerModeOn) return;
             if (Nameplates.ValidatePlayerAvatar(vrcPlayer)) {
                 if (RoomManager.field_Internal_Static_ApiWorld_0 != null && Config.EnableMasterFinder.Value && MasterIcon == null) {
                     if (PlayerManager.field_Private_Static_PlayerManager_0.field_Private_List_1_Player_0.Count > 1) {
-                        PlayerWrappers.GetEachPlayer(delegate(Player player) {
-                            player = vrcPlayer._player;
-                            if (vrcPlayer.field_Public_PlayerNameplate_0 == null)
-                                return;
-                            PlayerNameplate nameplate = vrcPlayer.field_Public_PlayerNameplate_0;
-                            oriFriendMarker = nameplate.transform.Find("Contents/Friend Marker").gameObject;
-                            MasterIcon = UnityEngine.Object.Instantiate(oriFriendMarker, oriFriendMarker.transform.parent);
-                            r = MasterIcon.GetComponent<RectTransform>();
-                            MasterIcon.GetComponent<Image>().sprite = MintyResources.masterCrown;
-                            MasterIcon.gameObject.name = "Mint_MasterIcon";
-                            MasterIcon.SetActive(Config.EnableMasterFinder.Value);
-                        });
+                        foreach (var obj in PlayerManager.field_Private_Static_PlayerManager_0.field_Private_List_1_Player_0) {
+                            if (vrcPlayer.field_Public_PlayerNameplate_0 != null && vrcPlayer.field_Private_VRCPlayerApi_0.isMaster) {
+                                var nameplate = vrcPlayer.field_Public_PlayerNameplate_0;
+                                oriFriendMarker = nameplate.transform.Find("Contents/Friend Marker").gameObject;
+                                MasterIcon = UnityEngine.Object.Instantiate(oriFriendMarker, oriFriendMarker.transform.parent);
+                                r = MasterIcon.GetComponent<RectTransform>();
+                                MasterIcon.GetComponent<Image>().sprite = MintyResources.masterCrown;
+                                MasterIcon.gameObject.name = "Mint_MasterIcon";
+                                MasterIcon.SetActive(Config.EnableMasterFinder.Value);
+                            }
+                        }
                     } else {
                         MasterIcon.Destroy();
                         MasterIcon = null;
@@ -51,7 +50,7 @@ namespace MintMod.Functions {
         }
 
         internal override void OnUpdate() {
-            if (MasterIcon != null && (ModCompatibility.NameplateStats || ModCompatibility.ReMod) && r != null && !MintUserInterface.isOnStreamerMode) {
+            if (MasterIcon != null && (ModCompatibility.NameplateStats || ModCompatibility.ReMod) && r != null && !MintUserInterface.isStreamerModeOn) {
                 if (Patches.IsQMOpen)
                     r.anchoredPosition = new Vector3(0f, 90f, 0f);
                 else
@@ -60,7 +59,7 @@ namespace MintMod.Functions {
         }
 
         internal override void OnPrefSave() {
-            if (MasterIcon != null && !MintUserInterface.isOnStreamerMode)
+            if (MasterIcon != null && !MintUserInterface.isStreamerModeOn)
                 MasterIcon.SetActive(Config.EnableMasterFinder.Value);
             //try{ MasterIcon.SetActive(Config.EnableMasterFinder.Value); }catch{}
         }
