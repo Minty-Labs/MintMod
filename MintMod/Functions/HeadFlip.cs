30ceaac5cd5981a12d16fd833f9954e741ce3719
Lily <admin@mintlily.lgbt>
2022-01-09 19:40:16 -0500

    Remove Dumb

--- MintMod/Functions/HeadFlip.cs
+++ MintMod/Functions/HeadFlip.cs
@@ -1,7 +1,8 @@
-﻿using MintMod;
+﻿/*using MintMod;
 using MintMod.Reflections;
 using UnityEngine;
 using UnityEngine.XR;
+using VRC.DataModel;
 
 namespace MintMod.Functions {
     internal class HeadFlip : MintSubMod {
@@ -11,19 +12,28 @@ namespace MintMod.Functions {
         public static bool IsHeadflipEnabled;
 
         private NeckMouseRotator _neckMouseRotator;
+        private NeckRange _orginal, _funny = new (-300, 300, 0);
+        private bool _setOrginal;
 
         internal override void OnUpdate() {
             if (XRDevice.isPresent) {
                 if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.H))
                     IsHeadflipEnabled = !IsHeadflipEnabled;
                 if (IsHeadflipEnabled) {
-                    //VRC.UserCamera.UserCameraController.field_Internal_Static_UserCameraController_0
-                    //    .field_Public_Vector3_0 = new(float.MaxValue, float.MaxValue, float.MaxValue);
                     if (_neckMouseRotator == null)
-                        _neckMouseRotator = VRCVrCamera.field_Private_Static_VRCVrCamera_0.GetComponentInChildren<NeckMouseRotator>();
-                    _neckMouseRotator.field_Private_Vector3_1 = new(float.MaxValue, float.MaxValue, float.MaxValue);
+                        _neckMouseRotator = Object.FindObjectOfType<NeckMouseRotator>();
+                    if (!_setOrginal) {
+                        _setOrginal = true;
+                        _orginal = _neckMouseRotator.field_Public_NeckRange_0;
+                    }
+                    _neckMouseRotator.field_Public_NeckRange_0 = _funny;
+                }
+                else {
+                    if (_neckMouseRotator == null && _setOrginal)
+                        _neckMouseRotator.field_Public_NeckRange_0 = _orginal;
                 }
             }
         }
     }
 }
+*/
