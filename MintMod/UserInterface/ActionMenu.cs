9239f62d595566e34993042a07e2fc196a82a8af
Lily <admin@mintlily.lgbt>
2022-01-24 11:36:56 -0500

    Added TeleporterVR VRTP Toggle

--- MintMod/UserInterface/ActionMenu.cs
+++ MintMod/UserInterface/ActionMenu.cs
@@ -12,44 +12,54 @@ using MintMod.Resources;
 using MintyLoader;
 
 namespace MintMod.UserInterface {
-    class ActionMenu : MintSubMod {
+    internal class ActionMenu : MintSubMod {
+        public override string Name => "MintyActionMenu";
+        public override string Description => "Adds some Mint functions to the ActionMenu.";
+        
         private static readonly string[] AMApiOutdatedVersions = { "0.1.0", "0.1.2", "0.2.0", "0.2.1", "0.2.2", "0.2.3", "0.3.0", "0.3.1", "0.3.2", "0.3.3", "0.3.4" };
         // Target Version -> 0.3.5
-        public static bool hasAMApiInstalled, AMApiOutdated, hasStarted;
-        private static PedalOption Freeze, ESP, Jump, Fly, VRTP;
-        private static bool ranOnce;
+        private static bool ranOnce, hasAMApiInstalled, AMApiOutdated, hasStarted;
 
         internal override void OnUserInterface() {
-            if (ranOnce) return;
-            if (MelonHandler.Mods.Any(m => m.Info.Name.Equals("ActionMenuApi"))) {
+            if (MelonHandler.Mods.Any(m => m.Info.Name.Equals("ActionMenuApi")))
                 hasAMApiInstalled = true;
-                if (!Config.ActionMenuON.Value) return;
-                if (MelonHandler.Mods.Single(m => m.Info.Name.Equals("ActionMenuApi")).Info.Version.Equals(AMApiOutdatedVersions)) {
-                    AMApiOutdated = true;
-                    Con.Warn("ActionMenuApi Outdated. Older verions are not supported, please update ActionMenuApi to v0.3.5 or above");
-                    return;
-                } else BuildMenu();
-                ranOnce = true;
+            if (ranOnce/* || !MintCore.isDebug*/) return;
+            DoAction();
+        }
+
+        private static void DoAction() {
+            if (!Config.ActionMenuON.Value) return;
+            if (MelonHandler.Mods.Single(m => m.Info.Name.Equals("ActionMenuApi")).Info.Version.Equals(AMApiOutdatedVersions)) {
+                AMApiOutdated = true;
+                Con.Warn("ActionMenuApi Outdated. Older versions are not supported, please update ActionMenuApi to v0.3.5 or above");
+                return;
             }
+            BuildMenu();
+            ranOnce = true;
         }
 
         private static void BuildMenu() {
             ActionMenuUtils.subMenu = VRCActionMenuPage.AddSubMenu(ActionMenuPage.Main, "<color=#82ffbe>MintMenu</color>", () => {
-                Freeze = CustomSubMenu.AddToggle("Freeze", false, PhotonFreeze.ToggleFreeze, MintyResources.FreezeIcon);
+                CustomSubMenu.AddToggle("Freeze", PhotonFreeze.isCurrentlyFreezing, PhotonFreeze.ToggleFreeze, MintyResources.FreezeIcon);
 
-                ESP = CustomSubMenu.AddToggle("ESP", false, Managers.ESP.PlayerESPState, MintyResources.ESPIcon);
+                CustomSubMenu.AddToggle("ESP", Managers.ESP.isESPEnabled, Managers.ESP.PlayerESPState, MintyResources.ESPIcon);
 
-                Jump = CustomSubMenu.AddButton("Jump", WorldActions.AddJump, MintyResources.JumpIcon);
+                CustomSubMenu.AddButton("Jump", WorldActions.AddJump, MintyResources.JumpIcon);
 
-                Fly = CustomSubMenu.AddToggle("Fly", false, Movement.NoClip, MintyResources.FlyIcon);
+                CustomSubMenu.AddToggle("Fly", Movement.FlightEnabled, Movement.NoClip, MintyResources.FlyIcon);
 
-                if (MelonHandler.Mods.FindIndex((MelonMod i) => i.Info.Name == "TeleporterVR") != -1)
-                    VRTP = CustomSubMenu.AddToggle("VR Teleport\nto Cursor", false, choice =>
+                if (MelonHandler.Mods.FindIndex((i) => i.Info.Name == "TeleporterVR") != -1)
+                    CustomSubMenu.AddToggle("VR Teleport\nto Cursor", TeleporterVR.Utils.VRUtils.active, choice =>
                         TeleporterVR.Utils.VRUtils.active = choice, MintyResources.VRTPIcon);
 
-                CustomSubMenu.AddRadialPuppet("Fly Speed", f => Movement.finalSpeed = f * 5, 0.2f);
+                //CustomSubMenu.AddRadialPuppet("Fly Speed", f => Movement.finalSpeed = f * 5, 0.2f);
             }, MintyResources.MintIcon2D);
             hasStarted = true;
+        }
+
+        internal override void OnPrefSave() {
+            if (!ranOnce && hasAMApiInstalled && Config.ActionMenuON.Value) 
+                DoAction();
         }
     }
 }
