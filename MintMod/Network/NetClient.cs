128ab33fdf8489e9a1e7e3bfbb7731ff3d77fd5b
Ellyvr <git@ellyvr.dev>
2022-04-04 18:00:02 -0500

    Mod based packets

---
+++ MintMod/Network/NetClient.cs
@@ -0,0 +1,19 @@
+﻿using MintyLoader;
+using WebSocketSharp;
+
+namespace MintMod.Network
+{
+    public class NetClient
+    {
+        public static void Initialize()
+        {
+            MintyNetClient._Client.OnMessage += ClientOnOnMessage;
+            MintyNetClient.ModLoaded = true;
+        }
+
+        private static void ClientOnOnMessage(object sender, MessageEventArgs e)
+        {
+            
+        }
+    }
+}
