330b591cfb8ebba511e2eee4dc2cc5caa8835070
Lily <admin@mintlily.lgbt>
2022-04-23 14:36:53 -0400

    Older Ui ReColor

--- MintMod/UserInterface/OldUI/ReColor.cs
+++ MintMod/UserInterface/OldUI/ReColor.cs
@@ -29,7 +29,6 @@ namespace MintMod.UserInterface.OldUI {
         internal override void OnStart() => Intstance = this;
 
         internal override void OnUserInterface() {
-            return;
             finalColor = Config.ColorGameMenu.Value ? Minty : defaultMenuColor();
             if (Config.ColorGameMenu.Value)
                 MelonCoroutines.Start(ColorMenu(finalColor));
@@ -59,7 +58,7 @@ namespace MintMod.UserInterface.OldUI {
                 #region normalColorImage
 
                 normalColorImage = new List<Image>();
-                GameObject quickMenu = UIWrappers.GetVRCUiMInstance().menuContent();
+                var quickMenu = UIWrappers.GetVRCUiMInstance().menuContent();
                 normalColorImage.Add(quickMenu.transform.Find("Screens/Settings_Safety/_Description_SafetyLevel").GetComponent<Image>());
                 normalColorImage.Add(quickMenu.transform.Find("Screens/Settings_Safety/_Buttons_SafetyLevel/Button_Custom/ON").GetComponent<Image>());
                 normalColorImage.Add(quickMenu.transform.Find("Screens/Settings_Safety/_Buttons_SafetyLevel/Button_None/ON").GetComponent<Image>());
@@ -74,15 +73,13 @@ namespace MintMod.UserInterface.OldUI {
                 normalColorImage.Add(quickMenu.transform.Find("Popups/InputPopup/InputField").GetComponent<Image>());
                 normalColorImage.Add(quickMenu.transform.Find("Popups/UpdateStatusPopup/Popup/InputFieldStatus").GetComponent<Image>());
                 normalColorImage.Add(quickMenu.transform.Find("Popups/AdvancedSettingsPopup/Popup/Panel").GetComponent<Image>());
-                //normalColorImage.Add(quickMenu.transform.Find("Popups/AddToPlaylistPopup/Popup/Panel").GetComponent<Image>());
-                normalColorImage.Add(quickMenu.transform.Find("Popups/BookmarkFriendPopup/Popup/Panel (2)").GetComponent<Image>());
-                normalColorImage.Add(quickMenu.transform.Find("Popups/EditPlaylistPopup/Popup/Panel").GetComponent<Image>());
+                normalColorImage.Add(quickMenu.transform.Find("Popups/AddToFavoriteListPopup/Popup/Panel").GetComponent<Image>());
+                normalColorImage.Add(quickMenu.transform.Find("Popups/EditFavoriteListPopup/Popup/Panel").GetComponent<Image>());
                 normalColorImage.Add(quickMenu.transform.Find("Popups/PerformanceSettingsPopup/Popup/Panel").GetComponent<Image>());
                 normalColorImage.Add(quickMenu.transform.Find("Popups/AlertPopup/Lighter").GetComponent<Image>());
                 normalColorImage.Add(quickMenu.transform.Find("Popups/RoomInstancePopup/Popup/Panel").GetComponent<Image>());
                 normalColorImage.Add(quickMenu.transform.Find("Popups/ReportWorldPopup/Popup/Panel").GetComponent<Image>());
                 normalColorImage.Add(quickMenu.transform.Find("Popups/ReportUserPopup/Popup/Panel").GetComponent<Image>());
-                normalColorImage.Add(quickMenu.transform.Find("Popups/AddToAvatarFavoritesPopup/Popup/Panel").GetComponent<Image>());
                 normalColorImage.Add(quickMenu.transform.Find("Popups/SearchOptionsPopup/Popup/Panel (1)").GetComponent<Image>());
                 normalColorImage.Add(quickMenu.transform.Find("Popups/SendInvitePopup/SendInviteMenu/Panel").GetComponent<Image>());
                 normalColorImage.Add(quickMenu.transform.Find("Popups/RequestInvitePopup/RequestInviteMenu/Panel").GetComponent<Image>());
@@ -90,13 +87,12 @@ namespace MintMod.UserInterface.OldUI {
                 normalColorImage.Add(quickMenu.transform.Find("Popups/ChangeProfilePicPopup/Popup/PanelBackground").GetComponent<Image>());
                 normalColorImage.Add(quickMenu.transform.Find("Popups/ChangeProfilePicPopup/Popup/TitlePanel").GetComponent<Image>());
                 normalColorImage.Add(quickMenu.transform.Find("Screens/UserInfo/User Panel/PanelHeaderBackground").GetComponent<Image>());
-                //normalColorImage.Add(quickMenu.transform.Find("Screens/UserInfo/User Panel/Panel (1)").GetComponent<Image>());
-                foreach (Transform obj in quickMenu.GetComponentsInChildren<Transform>(true).Where(x => x.name.Contains("Panel_Header"))) {
-                    foreach (Image img in obj.GetComponentsInChildren<Image>())
+                foreach (var obj in quickMenu.GetComponentsInChildren<Transform>(true).Where(x => x.name.Contains("Panel_Header"))) {
+                    foreach (var img in obj.GetComponentsInChildren<Image>())
                         normalColorImage.Add(img);
                 }
-                foreach (Transform obj in quickMenu.GetComponentsInChildren<Transform>(true).Where(x => x.name.Contains("Handle"))) {
-                    foreach (Image img in obj.GetComponentsInChildren<Image>())
+                foreach (var obj in quickMenu.GetComponentsInChildren<Transform>(true).Where(x => x.name.Contains("Handle"))) {
+                    foreach (var img in obj.GetComponentsInChildren<Image>())
                         normalColorImage.Add(img);
                 }
                 try {
@@ -117,14 +113,14 @@ namespace MintMod.UserInterface.OldUI {
                 #region dimmerColorImage
 
                 dimmerColorImage = new List<Image>();
-                GameObject quickMenu = UIWrappers.GetVRCUiMInstance().menuContent();
+                var quickMenu = UIWrappers.GetVRCUiMInstance().menuContent();
                 dimmerColorImage.Add(quickMenu.transform.Find("Screens/Settings_Safety/_Buttons_SafetyLevel/Button_Custom/ON/TopPanel_SafetyLevel").GetComponent<Image>());
                 dimmerColorImage.Add(quickMenu.transform.Find("Screens/Settings_Safety/_Buttons_SafetyLevel/Button_None/ON/TopPanel_SafetyLevel").GetComponent<Image>());
                 dimmerColorImage.Add(quickMenu.transform.Find("Screens/Settings_Safety/_Buttons_SafetyLevel/Button_Normal/ON/TopPanel_SafetyLevel").GetComponent<Image>());
                 dimmerColorImage.Add(quickMenu.transform.Find("Screens/Settings_Safety/_Buttons_SafetyLevel/Button_Maxiumum/ON/TopPanel_SafetyLevel").GetComponent<Image>());
                 dimmerColorImage.Add(quickMenu.transform.Find("Popups/ChangeProfilePicPopup/Popup/BorderImage").GetComponent<Image>());
-                foreach (Transform obj in quickMenu.GetComponentsInChildren<Transform>(true).Where(x => x.name.Contains("Fill"))) {
-                    foreach (Image img in obj.GetComponentsInChildren<Image>())
+                foreach (var obj in quickMenu.GetComponentsInChildren<Transform>(true).Where(x => x.name.Contains("Fill"))) {
+                    foreach (var img in obj.GetComponentsInChildren<Image>())
                         if (img.gameObject.name != "Checkmark")
                             dimmerColorImage.Add(img);
                 }
@@ -136,29 +132,27 @@ namespace MintMod.UserInterface.OldUI {
                 #region darkerColorImage
 
                 darkerColorImage = new List<Image>();
-                GameObject quickMenu = UIWrappers.GetVRCUiMInstance().menuContent();
+                var quickMenu = UIWrappers.GetVRCUiMInstance().menuContent();
                 darkerColorImage.Add(quickMenu.transform.Find("Popups/InputKeypadPopup/Rectangle").GetComponent<Image>());
                 darkerColorImage.Add(quickMenu.transform.Find("Popups/StandardPopupV2/Popup/BorderImage").GetComponent<Image>());
                 darkerColorImage.Add(quickMenu.transform.Find("Popups/StandardPopup/Rectangle").GetComponent<Image>());
                 darkerColorImage.Add(quickMenu.transform.Find("Popups/StandardPopup/MidRing").GetComponent<Image>());
                 darkerColorImage.Add(quickMenu.transform.Find("Popups/UpdateStatusPopup/Popup/BorderImage").GetComponent<Image>());
                 darkerColorImage.Add(quickMenu.transform.Find("Popups/AdvancedSettingsPopup/Popup/BorderImage").GetComponent<Image>());
-                darkerColorImage.Add(quickMenu.transform.Find("Popups/AddToPlaylistPopup/Popup/BorderImage").GetComponent<Image>());
-                darkerColorImage.Add(quickMenu.transform.Find("Popups/BookmarkFriendPopup/Popup/BorderImage").GetComponent<Image>());
-                darkerColorImage.Add(quickMenu.transform.Find("Popups/EditPlaylistPopup/Popup/BorderImage").GetComponent<Image>());
+                darkerColorImage.Add(quickMenu.transform.Find("Popups/AddToFavoriteListPopup/Popup/BorderImage").GetComponent<Image>());
+                darkerColorImage.Add(quickMenu.transform.Find("Popups/EditFavoriteListPopup/Popup/BorderImage").GetComponent<Image>());
                 darkerColorImage.Add(quickMenu.transform.Find("Popups/PerformanceSettingsPopup/Popup/BorderImage").GetComponent<Image>());
                 darkerColorImage.Add(quickMenu.transform.Find("Popups/RoomInstancePopup/Popup/BorderImage").GetComponent<Image>());
                 darkerColorImage.Add(quickMenu.transform.Find("Popups/RoomInstancePopup/Popup/BorderImage (1)").GetComponent<Image>());
                 darkerColorImage.Add(quickMenu.transform.Find("Popups/ReportWorldPopup/Popup/BorderImage").GetComponent<Image>());
                 darkerColorImage.Add(quickMenu.transform.Find("Popups/ReportUserPopup/Popup/BorderImage").GetComponent<Image>());
-                darkerColorImage.Add(quickMenu.transform.Find("Popups/AddToAvatarFavoritesPopup/Popup/BorderImage").GetComponent<Image>());
                 darkerColorImage.Add(quickMenu.transform.Find("Popups/SearchOptionsPopup/Popup/BorderImage").GetComponent<Image>());
                 darkerColorImage.Add(quickMenu.transform.Find("Popups/SendInvitePopup/SendInviteMenu/BorderImage").GetComponent<Image>());
                 darkerColorImage.Add(quickMenu.transform.Find("Popups/RequestInvitePopup/RequestInviteMenu/BorderImage").GetComponent<Image>());
                 darkerColorImage.Add(quickMenu.transform.Find("Popups/ControllerBindingsPopup/Popup/BorderImage").GetComponent<Image>());
                 darkerColorImage.Add(quickMenu.transform.Find("Screens/UserInfo/ModerateDialog/Panel/BorderImage").GetComponent<Image>());
-                foreach (Transform obj in quickMenu.GetComponentsInChildren<Transform>(true).Where(x => x.name.Contains("Background") && x.name != "PanelHeaderBackground" && !x.transform.parent.name.Contains("UserIcon"))) {
-                    foreach (Image img in obj.GetComponentsInChildren<Image>())
+                foreach (var obj in quickMenu.GetComponentsInChildren<Transform>(true).Where(x => x.name.Contains("Background") && x.name != "PanelHeaderBackground" && !x.transform.parent.name.Contains("UserIcon"))) {
+                    foreach (var img in obj.GetComponentsInChildren<Image>())
                         darkerColorImage.Add(img);
                 }
 
@@ -169,10 +163,10 @@ namespace MintMod.UserInterface.OldUI {
                 #region normalColorText
 
                 normalColorText = new List<Text>();
-                GameObject quickMenu = UIWrappers.GetVRCUiMInstance().menuContent();
-                foreach (Text txt in quickMenu.transform.Find("Popups/InputPopup/Keyboard/Keys").GetComponentsInChildren<Text>(true))
+                var quickMenu = UIWrappers.GetVRCUiMInstance().menuContent();
+                foreach (var txt in quickMenu.transform.Find("Popups/InputPopup/Keyboard/Keys").GetComponentsInChildren<Text>(true))
                     normalColorText.Add(txt);
-                foreach (Text txt in quickMenu.transform.Find("Popups/InputKeypadPopup/Keyboard/Keys").GetComponentsInChildren<Text>(true))
+                foreach (var txt in quickMenu.transform.Find("Popups/InputKeypadPopup/Keyboard/Keys").GetComponentsInChildren<Text>(true))
                     normalColorText.Add(txt);
                 normalColorText.Add(quickMenu.transform.Find("Screens/Settings/VolumePanel/VolumeGameWorld/Label").GetComponentInChildren<Text>(true));
                 normalColorText.Add(quickMenu.transform.Find("Screens/Settings/VolumePanel/VolumeGameVoice/Label").GetComponentInChildren<Text>(true));
@@ -185,17 +179,13 @@ namespace MintMod.UserInterface.OldUI {
             }
             
             foreach (var img in normalColorImage)
-                if (img != null)
-                    img.color = colorT;
+                img.color = colorT;
             foreach (var img in dimmerColorImage)
-                if (img != null)
-                    img.color = dimmerT;
+                img.color = dimmerT;
             foreach (var img in darkerColorImage)
-                if (img != null)
-                    img.color = darkerT;
+                img.color = darkerT;
             foreach (var txt in normalColorText)
-                if (txt != null)
-                    txt.color = color;
+                txt.color = color;
             
             var buttonTheme = new ColorBlock {
                 colorMultiplier = 1f,
@@ -304,7 +294,7 @@ namespace MintMod.UserInterface.OldUI {
             }
         }
 
-        public static IEnumerator DelayedHfxReColor(Color color) {
+        private static IEnumerator DelayedHfxReColor(Color color) {
             yield return new WaitForSeconds(5);
             Hfx = UnityEngine.Resources.FindObjectsOfTypeAll<HighlightsFXStandalone>();
             if (Hfx.Count == 0) yield break;
@@ -312,28 +302,27 @@ namespace MintMod.UserInterface.OldUI {
             HfxFound = true;
         }
 
-        private void ColorActionMenu(Color color) {
-            if (ModCompatibility.Styletor) return;
-            foreach (var grph in UnityEngine.Resources.FindObjectsOfTypeAll<PedalGraphic>())
-                grph.color = color;
-        }
+        //private void ColorActionMenu(Color color) {
+        //    if (ModCompatibility.Styletor) return;
+        //    foreach (var grph in UnityEngine.Resources.FindObjectsOfTypeAll<PedalGraphic>())
+        //        grph.color = color;
+        //}
 
         private void ColorLoadingScreenEnvironment(Color color) {
-            if (Config.ColorLoadingScreen.Value && MelonHandler.Mods.Any(i => i.Info.Name != "BetterLoadingScreen")) {
-                try {
-                    var quickMenu = UIWrappers.GetVRCUiMInstance().menuContent();
-                    var loadingBackground = quickMenu.transform.Find("Popups/LoadingPopup/3DElements/LoadingBackground_TealGradient/SkyCube_Baked").gameObject;
-                    loadingBackground.GetComponent<MeshRenderer>().material.SetTexture("_Tex", MintyResources.basicGradient);
-                    loadingBackground.GetComponent<MeshRenderer>().material.SetColor("_Tint", new Color(color.r / 2f, color.g / 2f, color.b / 2f, color.a));
-                    loadingBackground.GetComponent<MeshRenderer>().material.SetTexture("_Tex", MintyResources.basicGradient);
+            if (Config.ColorLoadingScreen.Value && MelonHandler.Mods.Any(i => i.Info.Name == "BetterLoadingScreen")) return;
+            try {
+                var quickMenu = UIWrappers.GetVRCUiMInstance().menuContent();
+                loadingBackground = quickMenu.transform.Find("Popups/LoadingPopup/3DElements/LoadingBackground_TealGradient/SkyCube_Baked").gameObject;
+                loadingBackground.GetComponent<MeshRenderer>().material.SetTexture("_Tex", MintyResources.basicGradient);
+                loadingBackground.GetComponent<MeshRenderer>().material.SetColor("_Tint", new Color(color.r / 2f, color.g / 2f, color.b / 2f, color.a));
+                loadingBackground.GetComponent<MeshRenderer>().material.SetTexture("_Tex", MintyResources.basicGradient);
 
-                    var initialLoadingBackground = GameObject.Find("LoadingBackground_TealGradient_Music/SkyCube_Baked");
-                    initialLoadingBackground.GetComponent<MeshRenderer>().material.SetTexture("_Tex", MintyResources.basicGradient);
-                    initialLoadingBackground.GetComponent<MeshRenderer>().material.SetColor("_Tint", new Color(color.r / 2f, color.g / 2f, color.b / 2f, color.a));
-                    initialLoadingBackground.GetComponent<MeshRenderer>().material.SetTexture("_Tex", MintyResources.basicGradient);
-                } catch (Exception e) {
-                    Con.Error(e);
-                }
+                initialLoadingBackground = GameObject.Find("LoadingBackground_TealGradient_Music/SkyCube_Baked");
+                initialLoadingBackground.GetComponent<MeshRenderer>().material.SetTexture("_Tex", MintyResources.basicGradient);
+                initialLoadingBackground.GetComponent<MeshRenderer>().material.SetColor("_Tint", new Color(color.r / 2f, color.g / 2f, color.b / 2f, color.a));
+                initialLoadingBackground.GetComponent<MeshRenderer>().material.SetTexture("_Tex", MintyResources.basicGradient);
+            } catch (Exception e) {
+                Con.Error(e);
             }
         }
     }
