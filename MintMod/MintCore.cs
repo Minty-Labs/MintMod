cc437c9bf3a008ceebee8d38c4fb9be30bc98507
Lily <admin@mintlily.lgbt>
2022-05-23 20:31:44 -0400

    Update ReMod.Core

--- MintMod/MintCore.cs
+++ MintMod/MintCore.cs
@@ -25,13 +25,13 @@ namespace MintMod {
             public const string Author = "Lily";
             public const string Company = "Minty Labs";
 #if !DEBUG
-            public const string Version = "2.31.7";
+            public const string Version = "2.31.8";
 #endif
 #if DEBUG
-            public const string Version = "2.29.0";
+            public const string Version = "2.XX.0";
 #endif
             public const string DownloadLink = null;
-            public const string UpdatedDate = "19 May 2022";
+            public const string UpdatedDate = "23 May 2022";
 #if !DEBUG
             internal const string LoaderVer = "2.7.5";
 #endif
