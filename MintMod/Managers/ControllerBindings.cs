ec675e6b9c1a1cdabc0b12b8fdef529be08695e4
Lily <admin@mintlily.lgbt>
2022-07-27 13:55:47 -0400

    Removal of all things VRChat

--- MintMod/Managers/ControllerBindings.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MintMod.Managers {
-    public class ControllerBindings {
-        public const string RightTrigger = "Oculus_CrossPlatform_SecondaryIndexTrigger";
-        public const string LeftTrigger = "Oculus_CrossPlatform_PrimaryIndexTrigger";
-    }
-}
